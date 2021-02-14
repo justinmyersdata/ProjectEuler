c39e6d0d0c1de5085414da4d5876510b81ab5fef
justinmyersdata <78823811+justinmyersdata@users.noreply.github.com>
2021-02-14 17:48:54 -0600

    Add files via upload

---
+++ Project_Euler_10.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 12 13:43:22 2021
+
+@author: MYERSJ1
+"""
+
+def isprime(x):
+    '''Returns True if x is prime and false if x is composite'''
+    
+    if x == 1:
+        return False
+    
+    elif x == 2:
+        return True
+    
+    elif x % 2 == 0:
+        return False
+    
+    else:
+        for y in range(3,int(x**(1/2))+1,2):    
+            if x % y == 0:        
+                return False
+        return True
+            
+def sumprime(x):
+    count = 0 
+    for number in range(x):
+        if isprime(number):
+            count+=number
+    return(count)
+        
+sumprime(2000000)    
